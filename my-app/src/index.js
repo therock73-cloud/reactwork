bd01820e712387c8391b4b5fa02d7d90d17987ce
therock73-cloud <saurabh.upadhyay_mca19@gla.ac.in>
2021-01-27 23:08:29 +0530

    10-use expression is jsx

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,9 @@ import ReactDom from 'react-dom';
 
 
 // render function only takes one jsx element
+const fname = "saurabh upadhyay";   // use this vaiable in jsx it can be done by curly breces
+
+
 
 ReactDom.render(
   //first method 
@@ -12,6 +15,8 @@ ReactDom.render(
 <h1>hellow from saurabh upadhyay to react</h1>
 <p>now i have lern to render multipal jsx eleiment by using div tag!! </p>
 <h2>now you can use any number of jsx eliments </h2>
+<h3>my name is {fname}</h3> 
+<p>refresh for rendom number = {Math.random()}</p>
 </>,
 // React.Fragment is used for remove axtra div  which is create becouse of div tag 
 //we can also use <> jsx eliments </> in placeof <div> or array 

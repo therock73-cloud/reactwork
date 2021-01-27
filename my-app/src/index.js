f6acc78810349491f459524c5a320768f067b71a
therock73-cloud <saurabh.upadhyay_mca19@gla.ac.in>
2021-01-27 21:43:40 +0530

    7how to use multipal jsx eliments in render function

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-ReactDom.render(<h1>hellow from saurabh upadhyay to react</h1>,
-  document.getElementById('root'));
+
+// render function only takes one jsx element
+
+ReactDom.render(
+  //first method 
+  //using <div> tag
+  //second method of using multipal jsx eleiments in render function by usng array
+[
+<h1>hellow from saurabh upadhyay to react</h1>,
+<p>now i have lern to render multipal jsx eleiment by using div tag!! </p>,
+<h2>now you can use any number of jsx eliments </h2>,
+],
+
+  document.getElementById('root')
+  );
 
   

3ce14fdbe102a4fde8d5a83a8de003b33ed7e5d2
therock73-cloud <saurabh.upadhyay_mca19@gla.ac.in>
2021-01-30 12:40:23 +0530

    12-complete second challange

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,7 +3,9 @@ import ReactDom from 'react-dom';
 
 
 // render function only takes one jsx element
-const fname = "saurabh upadhyay";   // use this vaiable in jsx it can be done by curly breces
+const fname = "saurabh";   // use this vaiable in jsx it can be done by curly breces
+const lname = "upadhyay";
+// by using templete litrals
 
 
 
@@ -12,11 +14,12 @@ ReactDom.render(
   //using <div> tag
   //second method of using multipal jsx eleiments in render function by usng array
 <>
-<h1>hellow from saurabh upadhyay to react</h1>
+<h1>Hellow from saurabh upadhyay to react</h1>
 <p>now i have lern to render multipal jsx eleiment by using div tag!! </p>
 <h2>now you can use any number of jsx eliments </h2>
 <h3>my name is {fname}</h3> 
 <p>refresh for rendom number = {Math.random()}</p>
+<p>my full name is by using templete litrals ={`${fname} ${lname}`}</p>
 </>,
 // React.Fragment is used for remove axtra div  which is create becouse of div tag 
 //we can also use <> jsx eliments </> in placeof <div> or array 

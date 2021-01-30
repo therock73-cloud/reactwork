6f770eaa4485fed04dc0da492ce45845baae1207
therock73-cloud <saurabh.upadhyay_mca19@gla.ac.in>
2021-01-30 15:48:57 +0530

    final with bsics

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css'
 
 
 // render function only takes one jsx element
@@ -14,12 +15,20 @@ ReactDom.render(
   //using <div> tag
   //second method of using multipal jsx eleiments in render function by usng array
 <>
-<h1>Hellow from saurabh upadhyay to react</h1>
+<h1 class="hedding">Hellow from saurabh upadhyay to react</h1>
 <p>now i have lern to render multipal jsx eleiment by using div tag!! </p>
 <h2>now you can use any number of jsx eliments </h2>
 <h3>my name is {fname}</h3> 
 <p>refresh for rendom number = {Math.random()}</p>
 <p>my full name is by using templete litrals ={`${fname} ${lname}`}</p>
+
+<h1 contentEditable>my name is saurabh upadhyay</h1>
+<img className = "img" src="https://picsum.photos/200/300
+ " alt="rendom img"/>
+
+
+
+
 </>,
 // React.Fragment is used for remove axtra div  which is create becouse of div tag 
 //we can also use <> jsx eliments </> in placeof <div> or array 

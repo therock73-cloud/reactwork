031af01e74db2a233bea7cbac5cdba1f4da6d277
therock73-cloud <saurabh.upadhyay_mca19@gla.ac.in>
2021-02-01 01:23:19 +0530

    33-complete buttone click count

---
+++ button-click-count/src/App.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+const App = () =>{
+    const [count,setCount] = useState(0);
+    const IN = () =>{
+        setCount(count +1);
+    };
+    return(
+    <>
+    <div  className="h11">
+    <h1>{count}</h1>
+    <button className="button" onClick={IN}>Click Me</button>
+    </div>
+    </>
+    )
+}
+export default App;
